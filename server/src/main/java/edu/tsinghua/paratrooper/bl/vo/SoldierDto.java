0f82bbfb2e60035b1bf7efceb544aa7b4e12e2d5
cuiods <cuihaoemail@163.com>
2019-06-23 10:14:31 +0800

    Modify vote message

---
+++ server/src/main/java/edu/tsinghua/paratrooper/bl/vo/SoldierDto.java
@@ -0,0 +1,14 @@
+package edu.tsinghua.paratrooper.bl.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SoldierDto {
+    private int candicate1Id;
+    private String candidate1Name;
+
+    private int candidate2Id;
+    private String candidate2Name;
+}
